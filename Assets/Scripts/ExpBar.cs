7596973b8db2ec2f74e1542aa6295404497b6169
Kyle Jackson <kyjackson@csumb.edu>
2021-11-04 15:04:34 -0700

    Bug fixes with expbar and ammo display

--- Assets/Scripts/ExpBar.cs
+++ Assets/Scripts/ExpBar.cs
@@ -1,25 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< Updated upstream
-
-public class ExpBar : MonoBehaviour
-{
-    public int value;           // The current exp
-    public int min;             // The Exp from the previous level
-    public int max;             // The Exp for the next level
-    public Slider expBar;
-    // Start is called before the first frame update
-    void Start()
-    {
-        expBar = GameObject.GetComponent<Slider>();
-        value = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().exp;
-        min = 0;
-        max = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().expNextLVL;
-        expBar.maxValue = max;
-        expBar.minValue = min;
-        expBar.value = value;
-=======
 using UnityEngine.UI;
 
 public class ExpBar : MonoBehaviour
@@ -28,6 +9,7 @@ public class ExpBar : MonoBehaviour
     public int value;           // The current exp
     public int min;             // The Exp from the previous level
     public int max;             // The Exp for the next level
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,33 +19,23 @@ public class ExpBar : MonoBehaviour
         expSlider.maxValue = max;
         expSlider.minValue = min;
         expSlider.value = value;
->>>>>>> Stashed changes
     }
 
     public void updateBar(int EXP) {
         value = EXP;
-<<<<<<< Updated upstream
-        GetComponent<Slider>().value = value;
-=======
+
         Debug.Log("Exp Gained");
         expSlider.value = value;
->>>>>>> Stashed changes
     }
 
     public void levelUp(int EXP, int nextLvl) {
         value = EXP;
         min = max;
         max = nextLvl;
-<<<<<<< Updated upstream
-        expBar.maxValue = max;
-        expBar.minValue = min;
-        expBar.value = value;
-    }
-}
-=======
+
         expSlider.maxValue = max;
         expSlider.minValue = min;
         expSlider.value = value;
     }
 }
->>>>>>> Stashed changes
+
