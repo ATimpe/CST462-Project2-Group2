f27aa37b7164c7330f61b99cdff8d865d1218759
ATimpe <47230161+ATimpe@users.noreply.github.com>
2021-11-20 16:02:26 -0800

    fixed exp issues

--- Assets/Scripts/VariableHolder.cs
+++ Assets/Scripts/VariableHolder.cs
@@ -15,7 +15,7 @@ public class VariableHolder : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.FindGameObjectsWithTag("VariableHolder")[0];
     }
 
     // Update is called once per frame
